87bb0a3f33ca30f2685e85fb86f99b71f8335638
lin0ks2 <132504799+lin0ks2@users.noreply.github.com>
2026-09-04 07:26:21 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,15 +2,15 @@
  * Проект: MOYAMOVA
  * Файл: sw.js
  * Назначение: Service Worker (PWA, офлайн, обновления)
- * Версия SW: 1.12.30
+ * Версия SW: 1.12.31
  * Обновлено: 2026-01-08
  * ========================================================== */
 
 'use strict';
 
 // Текущая версия SW / кэша
-const SW_VERSION = '1.12.30';
-const CACHE_NAME = 'moyamova-cache-v1.12.30';
+const SW_VERSION = '1.12.31';
+const CACHE_NAME = 'moyamova-cache-v1.12.31';
 
 // Преобразуем относительные пути в абсолютные URL на основе scope SW
 const toUrl = (path) => new URL(path, self.registration.scope).toString();
