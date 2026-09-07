c6ce39654f05fe3d679fd08c94b2c726d8b4146b
lin0ks2 <132504799+lin0ks2@users.noreply.github.com>
2026-09-07 14:03:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,8 +9,8 @@
 'use strict';
 
 // Текущая версия SW / кэша
-const SW_VERSION = '1.12.59';
-const CACHE_NAME = 'moyamova-cache-v1.12.59';
+const SW_VERSION = '1.12.60';
+const CACHE_NAME = 'moyamova-cache-v1.12.60';
 
 // Преобразуем относительные пути в абсолютные URL на основе scope SW
 const toUrl = (path) => new URL(path, self.registration.scope).toString();
